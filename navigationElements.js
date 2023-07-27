85473955ced3f7a70caf0c1912068cb967c66271
RareDestroyer8 <128938379+RareDestroyer8@users.noreply.github.com>
2023-07-26 20:44:58 -0700

    fevfr

---
+++ navigationElements.js
@@ -0,0 +1,89 @@
+let title = document.getElementById("header")
+
+var about = document.querySelector("#about");
+var pricing = document.querySelector("#pricing");
+var contact = document.querySelector("#contact");
+
+var mobileNavButton = document.querySelector("#mobileNavButton");
+var mobileNavButtonIcon = document.querySelector("#mobileNavButtonIcon");
+
+var mobileNavigator = true;
+
+if (window.matchMedia("(min-width: 64rem)").matches) {
+  moveNavElements();
+
+} else {
+  mobileMenu();
+}
+
+
+
+
+addEventListener("scroll", (e) => {
+  if (window.matchMedia("(min-width: 64rem)").matches) {
+    moveNavElements();
+  }
+      title.style.marginTop = window.scrollY * 1.05 + "px";
+
+});
+
+//Desktop
+function moveNavElements() {
+  if (window.scrollY > 20) {
+    about.classList.remove("aboutStart");
+    pricing.classList.remove("pricingStart");
+    contact.classList.remove("contactStart");
+    about.classList.add("aboutEnd");
+    pricing.classList.add("pricingEnd");
+    contact.classList.add("contactEnd");
+    console.log("1");
+  } else {
+    about.classList.remove("aboutEnd");
+    pricing.classList.remove("pricingEnd");
+    contact.classList.remove("contactEnd");
+    about.classList.add("aboutStart");
+    pricing.classList.add("pricingStart");
+    contact.classList.add("contactStart");
+  }
+}
+
+//Mobile
+function showMenu() {
+  mobileNavButton.classList.remove("mobileNavButtonShow");
+  mobileNavButtonIcon.classList.remove("mobileNavButtonIconShow");
+
+    mobileNavButton.classList.add("mobileNavButtonHide");
+    mobileNavButtonIcon.classList.add("mobileNavButtonIconHide");
+
+  about.classList.remove("aboutHidden");
+  pricing.classList.remove("pricingHidden");
+  contact.classList.remove("contactHidden");
+  about.classList.add("aboutShown");
+  pricing.classList.add("pricingShown");
+  contact.classList.add("contactShown");
+}
+
+function hideMenu() {
+  mobileNavButton.classList.remove("mobileNavButtonHide");
+  mobileNavButtonIcon.classList.remove("mobileNavButtonIconHide");
+
+  mobileNavButton.classList.add("mobileNavButtonShow");
+  mobileNavButtonIcon.classList.add("mobileNavButtonIconShow");
+
+  about.classList.remove("aboutShown");
+  pricing.classList.remove("pricingShown");
+  contact.classList.remove("contactShown");
+  about.classList.add("aboutHidden");
+  pricing.classList.add("pricingHidden");
+  contact.classList.add("contactHidden");
+}
+
+function mobileMenu() {
+  if (mobileNavigator == true) {
+    mobileNavigator = false;
+    hideMenu();
+  } else if (mobileNavigator == false) {
+    mobileNavigator = true;
+    showMenu();
+  }
+}
